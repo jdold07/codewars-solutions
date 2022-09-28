2fa21f7dff4fb937e6928c667e1a77e7d4142bec
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-09-29 02:00:39 +1000

    6kyu - Multiples of 3 or 5 (Review of JS)

--- 6 Kyu Kata/Multiples of 3 or 5/JS/Multiples of 3 or 5.js
+++ 6 Kyu Kata/Multiples of 3 or 5/JS/Multiples of 3 or 5.js
@@ -1,6 +1,9 @@
 //* My submitted solution
 const solution = (n) => (n > 0 ? [...Array(n).keys()].reduce((a, c, i) => a + (!(i % 3) || !(i % 5) ? c : 0), 0) : 0)
 
+//* Slight modification on review when organising to repo
+const solution2 = (n) => (n < 3 ? 0 : Array(n).reduce((a, _, i) => a + (!(i % 3) || !(i % 5) ? i : 0), 0))
+
 //* Solution from Kata test case - included for reference
 function _solution(number) {
   let sum = 0
