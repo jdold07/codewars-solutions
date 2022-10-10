3267b4b17471718eac52f98813dc2a6eab45defb
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-11 00:42:17 +1000

    Swift code file updates

--- kata-5-kyu/rgb-to-hex-conversion/swift/rgbToHexConversion.swift
+++ kata-5-kyu/rgb-to-hex-conversion/swift/rgbToHexConversion.swift
@@ -11,7 +11,7 @@ func rgb(_ r: Int, _ g: Int, _ b: Int) -> String {
       var n: Int = num
       if num < 0 { n = 0 }
       if num > 255 { n = 255 }
-      let result = ("0" + String(n, radix: 16).uppercased())
+      let result: (String) = ("0" + String(n, radix: 16).uppercased())
       return String(result[result.index(result.endIndex, offsetBy: -2)...])
     }
     return toHex(num: r) + toHex(num: g) + toHex(num: b)
--- kata-6-kyu/multiples-of-3-or-5/swift/multiplesOf3Or5.swift
+++ kata-6-kyu/multiples-of-3-or-5/swift/multiplesOf3Or5.swift
@@ -9,7 +9,7 @@
 func solution(_ num: Int) -> Int {
     var sum: Int = 0
     if num > 2 {
-      for (i) in Array(0...(num-1)) {
+      for (i: (Int)) in Array(0...(num-1)) {
         if (i % 3 == 0 || i % 5 == 0) {
           sum += i
         }
