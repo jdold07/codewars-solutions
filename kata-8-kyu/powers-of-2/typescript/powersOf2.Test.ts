5977c085ee7bc2f604683f9d9317ce60f26d6daf
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-11 20:22:49 +1000

    Added 57a083a57cb1f31db7000028 TS

---
+++ kata-8-kyu/powers-of-2/typescript/powersOf2.Test.ts
@@ -0,0 +1,30 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Powers of 2  [ ID: 57a083a57cb1f31db7000028 ] (powers-of-2)
+//+ URL: https://www.codewars.com/kata/57a083a57cb1f31db7000028
+//+ Category: REFERENCE  |  Tags: MATHEMATICS | FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+import { assert } from "chai"
+import { powersOfTwo } from "./powersOf2"
+import { shuffle as _shuffle } from "lodash"
+
+describe("Basic Tests", function () {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(powersOfTwo(0), [1])
+    assert.deepEqual(powersOfTwo(1), [1, 2])
+    assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16])
+  })
+})
+
+describe("Random Tests", function () {
+  const solution = (n: number): number[] => [...Array(n + 1)].map((x, y) => Math.pow(2, y))
+  const shuffle = _shuffle([...Array(201)].map((x, y) => y))
+
+  it("Testing for 201 random tests", () => {
+    for (const n in shuffle) {
+      assert.deepEqual(powersOfTwo(shuffle[n]), solution(shuffle[n]), `Testing for n = ${n}`)
+    }
+  })
+})
---
+++ kata-8-kyu/powers-of-2/typescript/powersOf2.ts
@@ -0,0 +1,13 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Powers of 2  [ ID: 57a083a57cb1f31db7000028 ] (powers-of-2)
+//+ URL: https://www.codewars.com/kata/57a083a57cb1f31db7000028
+//+ Category: REFERENCE  |  Tags: MATHEMATICS | FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+function powersOfTwo(n: number): number[] {
+  return Array.from({ length: n + 1 }, (_, i) => 2 ** i)
+}
+
+export { powersOfTwo }
