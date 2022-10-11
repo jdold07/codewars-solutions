edbe2912a1771e454dbff24c8d887e3543dec83f
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-11 23:25:44 +1000

    Corrected Tests - Must have got messed on import

--- kata-5-kyu/can-you-get-the-loop/javascript/canYouGetTheLoop.Test.js
+++ kata-5-kyu/can-you-get-the-loop/javascript/canYouGetTheLoop.Test.js
@@ -9,35 +9,38 @@
 const { expect } = require("chai")
 const { loop_size } = require("./canYouGetTheLoop")
 
-const list = create_chain(0, 1)
-
 describe("Tests", () => {
   it("test", () => {
+    let list = create_chain(0, 1)
     let result = loop_size(list)
     if (result != 1) {
       console.log("Expected 1 got " + result)
     }
     expect(result == 1)
 
+    list = create_chain(8778, 23)
     result = loop_size(list)
     if (result != 23) {
       console.log("Expected 23 got " + result)
     }
     expect(loop_size(list) == 23)
 
+    list = create_chain(23, 8778)
     result = loop_size(list)
     if (result != 8778) {
       console.log("Expected 8778 got " + result)
     }
     expect(loop_size(list) == 8778)
-    for (let i = 0; i < 2; i++) {
-      const b = Math.floor(Math.random() * 999) + 9000
 
+    for (let i = 0; i < 2; i++) {
+      const a = Math.floor(Math.random() * 999) + 9000
+      const b = Math.floor(Math.random() * 999) + 9000
+      list = create_chain(a, b)
       result = loop_size(list)
       if (result != b) {
         console.log("Expected " + b + " got " + result)
       }
-      expect(loop_size(list) == b)
+      expect(loop_size(list) === b)
     }
   })
 })
