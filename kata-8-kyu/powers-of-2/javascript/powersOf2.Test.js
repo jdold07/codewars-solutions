9353c1d3bd579cb71962e196ed3e015382639d79
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-11 20:22:40 +1000

    Added 57a083a57cb1f31db7000028 JS

---
+++ kata-8-kyu/powers-of-2/javascript/powersOf2.Test.js
@@ -0,0 +1,32 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Powers of 2  [ ID: 57a083a57cb1f31db7000028 ] (powers-of-2)
+//+ URL: https://www.codewars.com/kata/57a083a57cb1f31db7000028
+//+ Category: REFERENCE  |  Tags: MATHEMATICS | FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+const { assert, config } = require("chai")
+const { powersOfTwo } = require("./powersOf2")
+const _shuffle = require("lodash").shuffle
+
+config.truncateThreshold = 0
+
+describe("Basic Tests", function () {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(powersOfTwo(0), [1])
+    assert.deepEqual(powersOfTwo(1), [1, 2])
+    assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16])
+  })
+})
+
+describe("Random Tests", function () {
+  const solution = (n) => [...Array(n + 1)].map((x, y) => Math.pow(2, y))
+  const shuffle = _shuffle([...Array(201)].map((x, y) => y))
+
+  it("Testing for 201 random tests", () => {
+    for (const n in shuffle) {
+      assert.deepEqual(powersOfTwo(shuffle[n]), solution(shuffle[n]), `Testing for n = ${n}`)
+    }
+  })
+})
