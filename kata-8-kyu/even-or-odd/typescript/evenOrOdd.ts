de6393a0cbff48570cda66812554b3299bd743e8
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-15 15:26:29 +1000

    Add 53da3dbb4a5168369a0000fe TS

---
+++ kata-8-kyu/even-or-odd/typescript/evenOrOdd.ts
@@ -0,0 +1,13 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Even or Odd  [ ID: 53da3dbb4a5168369a0000fe ] (even-or-odd)
+//+ URL: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+//+ Category: REFERENCE  |  Tags: MATHEMATICS | FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+function even_or_odd(n: number): string {
+  return n % 2 ? "Odd" : "Even"
+}
+
+export { even_or_odd }
