83278dd9d4549aadc1ee653b782d66143dcec885
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-16 00:31:14 +1000

    Add 55685cd7ad70877c23000102 TS

---
+++ kata-8-kyu/return-negative/typescript/returnNegative.Test.ts
@@ -0,0 +1,27 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Return Negative  [ ID: 55685cd7ad70877c23000102 ] (return-negative)
+//+ URL: https://www.codewars.com/kata/55685cd7ad70877c23000102
+//+ Category: REFERENCE  |  Tags: FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+import { assert } from "chai"
+import { makeNegative } from "./returnNegative"
+
+describe("makeNegative", function () {
+  it("Sample tests", function () {
+    assert.equal(makeNegative(42), -42)
+    assert.equal(makeNegative(-9), -9)
+    assert.equal(makeNegative(0), 0)
+    assert.equal(makeNegative(1), -1)
+    assert.equal(makeNegative(-1), -1)
+  })
+  it("Random tests", function () {
+    for (let i = 0; i < 100; ++i) {
+      const n = ~~(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1)
+      const sol = n < 0 ? n : -n
+      assert.equal(makeNegative(n), sol, `Testing for num = ${n}`)
+    }
+  })
+})
---
+++ kata-8-kyu/return-negative/typescript/returnNegative.ts
@@ -0,0 +1,14 @@
+//+ ====================================================================================================================
+//+
+//+ 8 kyu - Return Negative  [ ID: 55685cd7ad70877c23000102 ] (return-negative)
+//+ URL: https://www.codewars.com/kata/55685cd7ad70877c23000102
+//+ Category: REFERENCE  |  Tags: FUNDAMENTALS
+//+
+//+ ====================================================================================================================
+
+const makeNegative = (num: number): number => {
+  return num > 0 ? num * -1 : num
+}
+
+
+export { makeNegative }
