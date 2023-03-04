a2cb603e1a4c95d5e6a7a189624d71be149d7564
jdold07 <tim@tpstech.dev>
2023-03-04 21:09:16 +1000

    build: 📦️ update dependencies, tsconfig & eslintrc
    - Update dependencies to latest versions
    - Remove unnecessary dependencies
    - Update tsconfig to use es2021
    - Update eslintrc to remove uninstalled plugins & use es2022

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,11 @@ module.exports = {
       },
       files: ["*.tsx", "*.ts"],
       parser: "@typescript-eslint/parser",
-      plugins: ["jest", "regexp", "@typescript-eslint"],
+      plugins: ["jest", "@typescript-eslint"],
       extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:regexp/recommended"
+        "plugin:@typescript-eslint/eslint-recommended"
       ],
       parserOptions: {
         ecmaVersion: "latest",
@@ -37,57 +36,13 @@ module.exports = {
         "jest/globals": true
       },
       files: ["*.jsx", "*.js", "*.mjs", "*.cjs"],
-      plugins: ["jest", "regexp"],
-      extends: ["eslint:recommended", "plugin:regexp/recommended"],
+      plugins: ["jest"],
+      extends: ["eslint:recommended"],
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module"
       },
       rules: { "no-extra-semi": 0, "no-fallthrough": 0 }
-    },
-    // Markdown
-    {
-      env: {
-        es2021: true,
-        node: true
-      },
-      files: ["*.md"],
-      parser: "eslint-plugin-markdown",
-      plugins: ["markdown"],
-      extends: ["plugin:markdown/recommended"],
-      processor: "markdown/markdown",
-      parserOptions: {},
-      rules: {}
-    },
-    // JSON
-    {
-      env: {
-        es2021: true,
-        node: true
-      },
-      files: ["*.json"],
-      parser: "eslint-plugin-json",
-      plugins: ["json"],
-      extends: ["plugin:json/recommended"],
-      parserOptions: {},
-      rules: {}
     }
-    // Test Files //! Disabled due to too much linting because they're not Jest specific layouts
-    //TODO - Need to look at this again when everything is working OK
-    // {
-    //   env: {
-    //     es2021: true,
-    //     node: true,
-    //     "jest/globals": true
-    //   },
-    //   files: ["*.Test.js", "*.Test.ts", "*.Test.mjs", "*.Test.coffee"],
-    //   plugins: ["jest"],
-    //   extends: ["eslint:recommended", "plugin:jest/recommended"],
-    //   parserOptions: {
-    //     ecmaVersion: "latest",
-    //     sourceType: "module"
-    //   },
-    //   rules: {}
-    // }
   ]
 }
